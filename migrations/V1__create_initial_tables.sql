521ce7e3351ba61fbf1a42882b00cc1cfd6b678d
n1kolaypython <sh4retime@yandex.ru>
2024-03-16 12:32:03 +0300

    Modified migrations

--- migrations/V1__create_initial_tables.sql
+++ migrations/V1__create_initial_tables.sql
@@ -2,42 +2,42 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS clients (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
     full_name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(55) UNIQUE NOT NULL,
     email VARCHAR(512) UNIQUE DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS pets (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
     nickname VARCHAR(125),
     species VARCHAR(125) NOT NULL,
     breed VARCHAR(125),
-    owner_id UUID REFERENCES clients(id) ON DELETE SET NULL ON UPDATE SET NULL
+    owner_id VARCHAR REFERENCES clients(id) ON DELETE SET NULL ON UPDATE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS employees (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
     full_name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(55) UNIQUE NOT NULL,
-    email VARCHAR(512) UNIQUE
+    email VARCHAR(512) UNIQUE DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS appointments (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
     cabinet VARCHAR(55) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
 
-    client_id UUID REFERENCES clients(id) ON DELETE SET NULL ON UPDATE SET NULL,
-    employee_id UUID REFERENCES employees(id) ON DELETE SET NULL ON UPDATE SET NULL,
-    pet_id UUID REFERENCES pets(id) ON DELETE SET NULL ON UPDATE SET NULL
+    client_id VARCHAR REFERENCES clients(id) ON DELETE SET NULL ON UPDATE SET NULL,
+    employee_id VARCHAR REFERENCES employees(id) ON DELETE SET NULL ON UPDATE SET NULL,
+    pet_id VARCHAR REFERENCES pets(id) ON DELETE SET NULL ON UPDATE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS client_payments (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    appointment_id UUID REFERENCES appointments(id) ON DELETE SET NULL ON UPDATE RESTRICT,
-    total DECIMAL(7, 2) NOT NULL,
+    id VARCHAR PRIMARY KEY DEFAULT uuid_generate_v4(),
+    appointment_id VARCHAR REFERENCES appointments(id) ON DELETE SET NULL ON UPDATE RESTRICT,
+    total VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
